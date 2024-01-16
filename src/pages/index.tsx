f2f698f41c14fd0a276b7fc47088de4214d2238b
ozaki <fs65ugumori@yahoo.co.jp>
2024-01-16 21:40:50 +0900

    スワイプとボタン両方で切り替えられるようになった

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
 import Head from 'next/head';
 import IframeList from '~/components/IframeList';
 
+const list = [
+  {
+    url: 'https://www.youtube.com/embed/1Mi-_Plz5as?si=qUI-ZiHkK9PE0IEG',
+    memo: 'アクセシビリティカンファレンス福岡の会場で放映されてたマーカッポの動画\n会場で大ウケだった\nマークアップに携わる人なら誰でも楽しめる内容',
+  },
+  {
+    url: 'https://www.youtube.com/embed/qq9R7-1FptQ?si=Y3GBJHltOghp44fp',
+    memo: 'マーカッポの1回目の動画\n面白い',
+  },
+  {
+    url: 'https://www.youtube.com/embed/2H49N5e7hPw?si=JIFi6VrwUkMIqZFi',
+    memo: 'マーカッポ#2の振り返り動画\nマーカッポが勉強になるとつぶやいたのを拾ってもらえて、まじめに受け取るなと言われてしまいました',
+  },
+];
+
 export default function Home() {
   return (
     <>
@@ -22,7 +37,7 @@ export default function Home() {
         <p>ああああ</p>
         <p>ああああ</p>
         <p>ああああ</p>
-        <IframeList />
+        <IframeList list={list} defaultIndex={1} />
       </main>
     </>
   );
