a3f136a675c7c72d865685a7cd79656d571adbd8
ozaki <fs65ugumori@yahoo.co.jp>
2024-01-17 00:08:49 +0900

    スクロール系の処理をhooksに切り出し

--- src/components/IframeList/index.tsx
+++ src/components/IframeList/index.tsx
@@ -1,12 +1,36 @@
-import {
-  AnimationPlaybackControls,
-  animate,
-  useMotionValueEvent,
-  useScroll,
-} from 'framer-motion';
 import IframeItem from '~/components/IframeItem';
 import style from './style.module.css';
-import { useEffect, useRef, useState } from 'react';
+import useSwiper from '~/hooks/useSwiper';
+
+type Props = {
+  list: {
+    url: string;
+    memo: string;
+  }[];
+  defaultIndex?: number;
+};
+
+function IframeList({ list, defaultIndex }: Props) {
+  const { toPrev, toNext, containerRef, isFirst, isLast, selectedIndex } =
+    useSwiper({
+      index: defaultIndex,
+      listSize: list.length,
+    });
+  return (
+    <section className={style.IframeList}>
+      <p>index: {selectedIndex}</p>
+      <div className={style.IframeList__main}>
+        {!isFirst && <PrevButton onClick={toPrev} />}
+        {!isLast && <NextButton onClick={toNext} />}
+        <div ref={containerRef} className={style.IframeList__container}>
+          {list.map(({ url, memo }, index) => (
+            <IframeItem key={index} url={url} memo={memo} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
 
 type PrevButtonProps = {
   onClick: () => void;
@@ -39,97 +63,6 @@ function NextButton({ onClick }: NextButtonProps) {
     >
       ＞
     </button>
-  );
-}
-
-type Props = {
-  list: {
-    url: string;
-    memo: string;
-  }[];
-  defaultIndex?: number;
-};
-
-function IframeList({ list, defaultIndex }: Props) {
-  const [selectedIndex, setSelectedIndex] = useState<number>(defaultIndex ?? 0);
-
-  const containerRef = useRef<HTMLDivElement>(null);
-  const animationRef = useRef<AnimationPlaybackControls | null>(null);
-
-  const { scrollXProgress } = useScroll({ container: containerRef });
-
-  const getCurrentIndex = (x: number): number => {
-    // xは横スクロール全体の幅を1とした時の現在の位置(0〜1.0)
-    // scroll-snapでスナップされる位置と同じように、1枚目の半分を超えたら1、2枚目の半分を超えたら2、といった値を返す
-    return Math.round(x / (1 / (list.length - 1)));
-  };
-
-  const scroll = (nextIndex: number) => {
-    const container = containerRef.current;
-    if (!container) return;
-
-    animationRef.current?.stop();
-    animationRef.current = animate(
-      container.scrollLeft,
-      container.scrollWidth * (nextIndex / list.length),
-      {
-        duration: 0.3,
-        onUpdate: (v) => {
-          console.log('onUpdate', v);
-          container.scrollLeft = v;
-        },
-        onPlay: () => {
-          console.log('onPlay');
-          // アニメーション中はスナップを無効ししないとカクついてしまう
-          container.style.scrollSnapType = 'none';
-        },
-        onComplete: () => {
-          console.log('onComplete');
-          container.style.scrollSnapType = '';
-          animationRef.current = null;
-        },
-      },
-    );
-  };
-
-  const onClickPrev = () => {
-    setSelectedIndex(selectedIndex - 1);
-    scroll(selectedIndex - 1);
-  };
-
-  const onClickNext = () => {
-    setSelectedIndex(selectedIndex + 1);
-    scroll(selectedIndex + 1);
-  };
-
-  useMotionValueEvent(scrollXProgress, 'change', (x) => {
-    if (animationRef.current) return;
-    // スクロールで切り替えた時にindexの同期をとる
-    setSelectedIndex(getCurrentIndex(x));
-  });
-
-  useEffect(() => {
-    if (containerRef.current && defaultIndex) {
-      containerRef.current.scrollLeft =
-        containerRef.current.scrollWidth * (defaultIndex / list.length);
-    }
-  }, [defaultIndex]);
-
-  return (
-    <section className={style.IframeList}>
-      <p>index: {selectedIndex}</p>
-      <div>
-        {selectedIndex > 0 && <PrevButton onClick={onClickPrev} />}
-        {selectedIndex < list.length - 1 && (
-          <NextButton onClick={onClickNext} />
-        )}
-        <div ref={containerRef} className={style.IframeList__container}>
-          {list.map(({ url, memo }, index) => (
-            <IframeItem key={index} url={url} memo={memo} />
-          ))}
-        </div>
-      </div>
-    </section>
   );
 }
 
---
+++ src/hooks/useSwiper.ts
@@ -0,0 +1,113 @@
+import {
+  AnimationPlaybackControls,
+  animate,
+  useMotionValueEvent,
+  useScroll,
+} from 'framer-motion';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+type Props = {
+  listSize: number;
+  index?: number;
+};
+
+function useSwiper({ listSize, index }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState<number>(index ?? 0);
+
+  /**
+   * scroll-snap-typeを付与する要素にセットする
+   * その要素の子要素にはsnap対象のリストを配置する
+   */
+  const containerRef = useRef<HTMLDivElement>(null);
+  const animationRef = useRef<AnimationPlaybackControls | null>(null);
+
+  const { scrollXProgress } = useScroll({ container: containerRef });
+
+  const isFirst = selectedIndex === 0;
+  const isLast = selectedIndex === listSize - 1;
+
+  /**
+   * 現在のスクロール位置がどのindexに該当するかか返す
+   * xは横スクロール全体の幅を1とした時の現在の位置(0〜1.0)
+   * scroll-snapでスナップされる位置と同じように、1枚目の半分を超えたら1、2枚目の半分を超えたら2、といった値を返す
+   */
+  const getCurrentIndex = (x: number): number => {
+    return Math.round(x / (1 / (listSize - 1)));
+  };
+
+  /**
+   * 引数で渡したindexの位置までアニメーションつきでスクロールさせる
+   */
+  const scrollTo = (nextIndex: number) => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    animationRef.current?.stop();
+    animationRef.current = animate(
+      container.scrollLeft,
+      container.scrollWidth * (nextIndex / listSize),
+      {
+        duration: 0.3,
+        onUpdate: (v) => {
+          container.scrollLeft = v;
+        },
+        onPlay: () => {
+          // アニメーション中はスナップを無効ししないとカクついてしまう
+          container.style.scrollSnapType = 'none';
+        },
+        onComplete: () => {
+          container.style.scrollSnapType = '';
+          animationRef.current = null;
+        },
+      },
+    );
+  };
+
+  /**
+   * 前のindexにスクロールさせる
+   */
+  const toPrev = useCallback(() => {
+    if (isFirst) return;
+    setSelectedIndex(selectedIndex - 1);
+    scrollTo(selectedIndex - 1);
+  }, [selectedIndex]);
+
+  /**
+   * 次のindexにスクロールさせる
+   */
+  const toNext = useCallback(() => {
+    if (isLast) return;
+    setSelectedIndex(selectedIndex + 1);
+    scrollTo(selectedIndex + 1);
+  }, [selectedIndex]);
+
+  /**
+   * containerRefの要素のスクロール位置が変わる度に呼ばれてindeをさ最新化する
+   */
+  useMotionValueEvent(scrollXProgress, 'change', (x) => {
+    if (animationRef.current) return;
+    // スクロールで切り替えた時にindexの同期をとる
+    setSelectedIndex(getCurrentIndex(x));
+  });
+
+  /**
+   * 初期表示のindexが指定されていたらそこまでスクロール位置をずらす
+   */
+  useEffect(() => {
+    const container = containerRef.current;
+    if (container && index) {
+      container.scrollLeft = container.scrollWidth * (index / listSize);
+    }
+  }, [index]);
+
+  return {
+    toPrev,
+    toNext,
+    containerRef,
+    isFirst,
+    isLast,
+    selectedIndex,
+  };
+}
+
+export default useSwiper;
